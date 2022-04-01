c1e306db2c56db5e90e09bb1d0e12dab0db0b9e6
juan <juankmiloh@hotmail.com>
2022-04-01 14:26:34 -0500

    Create service Linux

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ venom
 
 function start(client) {
   client.onMessage((message) => {
-    console.log('message :>> ', message)
+    // console.log('message :>> ', message)
     client
       .sendText(message.from, `Hola ${message.chat.contact.displayName}`+' bienvenido al WhatsApp de la mesa de ayuda de la Superintendencia' +
       ' de Servicios Públicos Domiciliarios. Tu información es muy importante para nosotros, por eso te invitamos' +
       ' a que validemos juntos los datos de acceso al Sistema Único de Información, así podremos continuar gestionando' + 
       ' tu solicitud, para continuar por favor compártenos el identificador de tu empresa')
       .then((result) => {
-        console.log('Result: ', result); //return object success
+        // console.log('Result: ', result); //return object success
+        console.log('Result: ', result.status); //return object success
       })
       .catch((erro) => {
         console.error('Error when sending: ', erro); //return object error
---
+++ server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# echo run suibot
+node app.js
