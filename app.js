f6071de9c4ed0f3d89926929e6b6853d759946eb
juan <juankmiloh@hotmail.com>
2022-04-01 14:38:54 -0500

    Export QR code

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 // Supports ES6
 // import { create, Whatsapp } from 'venom-bot';
+const fs = require('fs');
 const venom = require('venom-bot');
 
 venom
-  .create({
-    session: 'session-name', //name of session
-    multidevice: false // for version not multidevice use false.(default: true)
+  .create(
+    'sessionName',
+    (base64Qr, asciiQR, attempts, urlCode) => {
+      console.log(asciiQR); // Optional to log the QR in the terminal
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+      if (matches.length !== 3) {
+        return new Error('Invalid input string');
+      }
+      response.type = matches[1];
+      response.data = new Buffer.from(matches[2], 'base64');
+
+      var imageBuffer = response;
+      require('fs').writeFile(
+        'out.png',
+        imageBuffer['data'],
+        'binary',
+        function (err) {
+          if (err != null) {
+            console.log(err);
+          }
+        }
+      );
+    },
+    undefined,
+    { logQR: false }
+  )
+  .then((client) => {
+    start(client);
   })
-  .then((client) => start(client))
   .catch((erro) => {
     console.log(erro);
   });
@@ -16,10 +43,10 @@ function start(client) {
   client.onMessage((message) => {
     // console.log('message :>> ', message)
     client
-      .sendText(message.from, `Hola ${message.chat.contact.displayName}`+' bienvenido al WhatsApp de la mesa de ayuda de la Superintendencia' +
-      ' de Servicios Públicos Domiciliarios. Tu información es muy importante para nosotros, por eso te invitamos' +
-      ' a que validemos juntos los datos de acceso al Sistema Único de Información, así podremos continuar gestionando' + 
-      ' tu solicitud, para continuar por favor compártenos el identificador de tu empresa')
+      .sendText(message.from, `Hola ${message.chat.contact.displayName}` + ' bienvenido al WhatsApp de la mesa de ayuda de la Superintendencia' +
+        ' de Servicios Públicos Domiciliarios. Tu información es muy importante para nosotros, por eso te invitamos' +
+        ' a que validemos juntos los datos de acceso al Sistema Único de Información, así podremos continuar gestionando' +
+        ' tu solicitud, para continuar por favor compártenos el identificador de tu empresa')
       .then((result) => {
         // console.log('Result: ', result); //return object success
         console.log('Result: ', result.status); //return object success
